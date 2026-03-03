8bf3f8a3194908ff2fda05e279df067e8a319147
ThiagoMonteiroBSI <thiagodepaulamonteiro@gmail.com>
2026-03-02 21:03:57 -0300

    chore: configuracao de autenticacao JTW

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'core',
+    'finances',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', # Bloqueia toda a API por padrão
+    )
+}
