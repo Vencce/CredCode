e29927aa88e804f53fccbae5e2d1587911ce9675
vencce <vitorferreira20005@gmial.com>
2026-04-29 14:09:51 -0300

    feat: Add service worker and manifest for PWA support; update HTML language attribute

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,3 +12,11 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+    }).catch(error => {
+    })
+  })
+}
